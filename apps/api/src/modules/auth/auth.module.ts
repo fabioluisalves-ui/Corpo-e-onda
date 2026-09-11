6605b64a328d7907af99d14ba9a9baea5aa1a47b
fabioluisalves-ui <fabioluisalves@gmail.com>
2026-09-11 16:47:12 -0300

    Update auth.module.ts

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../../common/prisma.service';
 
+@Global()
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
